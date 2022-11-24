a772f8f49b1a87a25675945e021f7640d402391e
Linh Bui <linh@Linhs-iMac.local>
2022-11-24 12:49:49 -0800

    set present class for currentHour

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-$(document).ready(function () {
-
+$(document).ready(function() {
+     $('#currentDay').text(moment().format('dddd, MMMM Do')); 
+     let currentHour = moment().hour().valueOf();
+     
+     $('.time-block').each(function(){
+        let scheduleHour = parseInt($(this).attr('id').split('-')[1]);
+        if(currentHour==scheduleHour){
+            $(this).addClass('present')
+        }
+    });
 });
