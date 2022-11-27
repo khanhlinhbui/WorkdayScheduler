4b1359ec56ff074c64270f1b4dfbaf81289c8da1
Linh Bui <linh@Linhs-iMac.local>
2022-11-26 16:36:11 -0800

    add saving function when click save button

--- script.js
+++ script.js
@@ -1,14 +1,13 @@
-$(document).ready(function() {
-     $('#currentDay').text(moment().format('dddd, MMMM Do')); 
+$(document).ready(function(){
+    $('#currentDay').text(moment().format('dddd, MMMM Do')); 
      let currentHour = moment().hour().valueOf();
-     
-     $('.time-block').each(function(){
+     //  determine which time block is past, present or future
+    $('.time-block').each(function(){
         let scheduleHour = parseInt($(this).attr('id').split('-')[1]);
         if(currentHour==scheduleHour){
             $(this).addClass('present')
             $(this).removeClass('past')
             $(this).removeClass('future')
-
         }
         if(currentHour > scheduleHour){
             $(this).addClass('past')
@@ -21,4 +20,19 @@ $(document).ready(function() {
             $(this).removeClass('past')
         }
     });
+    $('.saveBtn').click(function(){
+        scheduleContentInput = $(this).siblings('.scheduleContent').val()
+        scheduleContentKey = $(this).parent().attr('id')
+        localStorage.setItem(scheduleContentKey,scheduleContentInput)
+    });
+    $('#time-9 .scheduleContent').val(localStorage.getItem('time-9'))
+    $('#time-10 .scheduleContent').val(localStorage.getItem('time-10'))
+    $('#time-11 .scheduleContent').val(localStorage.getItem('time-11'))
+    $('#time-12 .scheduleContent').val(localStorage.getItem('time-12'))
+    $('#time-13 .scheduleContent').val(localStorage.getItem('time-13'))
+    $('#time-14 .scheduleContent').val(localStorage.getItem('time-14'))
+    $('#time-15 .scheduleContent').val(localStorage.getItem('time-15'))
+    $('#time-16 .scheduleContent').val(localStorage.getItem('time-16'))
+    $('#time-17 .scheduleContent').val(localStorage.getItem('time-17'))
+
 });
